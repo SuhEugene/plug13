09f695aa1050151f86fa53dcdd92813b3df5e593
SuhEugene <suheugene@ya.ru>
2024-01-20 20:21:31 +0300

    Update random string generation

--- utils/simpleFunctions.ts
+++ utils/simpleFunctions.ts
@@ -1,6 +1,22 @@
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+export enum RndStrAlphabet {
+  useUpper = 1 << 0,
+  useLower = 1 << 1,
+  useNumbers = 1 << 2,
+  useAll = (1 << 3) - 1
+}
 
-export function randomString(length: number) {
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+
+export function randomString(alphabet: RndStrAlphabet, length: number) {
+  let characters = '';
+  if (!alphabet) throw Error('No alphabet options provided');
+
+  if (alphabet & RndStrAlphabet.useUpper)  characters += upper;
+  if (alphabet & RndStrAlphabet.useLower)  characters += lower;
+  if (alphabet & RndStrAlphabet.useNumbers) characters += numbers;
+
   let out = '';
   for (let i = 0; i < length; i++)
     out += characters[Math.floor(Math.random() * length)];
