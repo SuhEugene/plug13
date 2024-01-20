14acac21f1895f6618de5c69a523b44c61a434f0
SuhEugene <suheugene@ya.ru>
2024-01-20 19:32:09 +0300

    Add bulk logout endpoint

---
+++ server/api/auth.delete.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const user = await useAuth(event);
+  
+  await prisma.user.update({
+    data: { tokenVersion: { increment: 1 } },
+    where: { id: user.id }
+  })
+
+  return;
+})
