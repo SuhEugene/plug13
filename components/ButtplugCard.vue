daeef2008a3acddaa88762ebca9474adb5ab1db0
SuhEugene <suheugene@ya.ru>
2024-01-21 16:32:43 +0300

    Update buttplug page

--- components/ButtplugCard.vue
+++ components/ButtplugCard.vue
@@ -12,16 +12,16 @@ onUnmounted(() => disconnect());
 </script>
 
 <template>
-  <Card>
+  <Card class="flex flex-col">
     <CardHeader>
       <CardTitle class="flex flex-row items-center">
         <div class="flex-grow">Buttplug.io</div>
         <ButtonDialogDisconnectButtplug v-if="connected" />
       </CardTitle>
     </CardHeader>
-    <CardContent>
+    <CardContent class="h-full">
       <Transition name="fade-then-slide" mode="out-in">
-        <div v-if="!connected">
+        <div v-if="!connected" class="h-full flex flex-col items-center justify-center lg:pb-16">
           <p class="text-sm text-center font-bold">Подключение к Intiface Central</p>
           <Input class="block mx-auto mt-2 max-w-64" placeholder="ws://127.0.0.1:12345" :disabled="pending" v-model="wsAddress" />
           <Button class="block mx-auto mt-2" variant="secondary" :disabled="pending" @click="connect(wsAddress)">
@@ -30,11 +30,10 @@ onUnmounted(() => disconnect());
             <span>Подключиться</span>
           </Button>
         </div>
-        <div v-else>
-          <div v-for="device in devices">{{ device.name }}</div>
+        <div v-else-if="!devices.length" class="h-full flex flex-col items-center justify-center lg:pb-16">
           <Transition name="fade-then-slide" mode="out-in">
-            <p v-if="!devices.length && !isScanning" class="text-sm text-center text-muted-foreground">Устройства не найдены</p>
-            <p v-else-if="!devices.length && isScanning" class="text-sm text-center text-muted-foreground">
+            <p v-if="!isScanning" class="text-sm text-center text-muted-foreground">Устройства не найдены</p>
+            <p v-else-if="isScanning" class="text-sm text-center text-muted-foreground">
               <Loader2 class="w-4 h-4 mr-2 inline-block animate-spin" />
               <span>Поиск устройств...</span>
             </p>
@@ -47,15 +46,28 @@ onUnmounted(() => disconnect());
             <span>Завершить сканирование</span>
           </Button>
         </div>
-      </Transition>
-      <Transition name="fade-then-slide">
-        <Alert v-if="error" :key="error.message" variant="danger" class="mt-4">
-          <XCircle class="h-4 w-4" />
-          <AlertTitle>Ошибка</AlertTitle>
-          <AlertDescription>{{ error.message }}</AlertDescription>
-        </Alert>
+        <div v-else>
+          <div v-for="device in devices" @click="device.vibrate(1);device.stop()" @dblclick="">{{ device.name }}</div>
+          <Button v-if="!isScanning" class="block mx-auto mt-2" variant="secondary" size="sm" @click="startScanning()">
+            <Search class="w-4 h-4 mr-2 inline-block" />
+            <span>Сканировать</span>
+          </Button>
+          <Button v-else class="block mx-auto mt-2" variant="secondary" size="sm" @click="stopScanning()">
+              <Loader2 class="w-4 h-4 mr-2 inline-block animate-spin" />
+            <span>Завершить сканирование</span>
+          </Button>
+        </div>
       </Transition>
     </CardContent>
+    <Transition name="fade-then-slide">
+      <CardFooter v-if="error" :key="error.message">
+          <Alert variant="danger" class="mt-4">
+            <XCircle class="h-4 w-4" />
+            <AlertTitle>Ошибка</AlertTitle>
+            <AlertDescription>{{ error.message }}</AlertDescription>
+          </Alert>
+      </CardFooter>
+    </Transition>
   </Card>
 </template>
 
--- components/ManagementPanel.vue
+++ components/ManagementPanel.vue
@@ -4,10 +4,10 @@
 <template>
   <div class="w-full flex flex-col lg:flex-row gap-4">
     <div class="flex flex-col gap-4 md:flex-row lg:flex-col lg:basis-[384px]">
-      <UserAccountCard class="flex-grow md:my-auto" />
+      <UserAccountCard class="flex-grow md:my-auto lg:my-0 lg:flex-grow-0" />
       <CodeStringCard class="md:max-w-sm lg:max-w-none" />
     </div>
-    <ButtplugCard class="flex-grow" />
+    <ButtplugCard class="flex-grow lg:min-h-[80vh]" />
   </div>
 </template>
 
