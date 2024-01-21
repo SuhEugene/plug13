28a5baace1f16c480a0d9c3867d31d122d481465
SuhEugene <suheugene@ya.ru>
2024-01-21 17:24:40 +0300

    Add success button variant

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -15,6 +15,8 @@ export const buttonVariants = cva(
             'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
             'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        success:
+              'bg-green-600 text-primary-foreground shadow hover:bg-green-600/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
