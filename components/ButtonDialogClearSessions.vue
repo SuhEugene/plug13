5f4761f7893c15ef17ef9b122e0565da158c20e4
SuhEugene <suheugene@ya.ru>
2024-01-21 15:39:01 +0300

    Add UserAccount card

---
+++ components/ButtonDialogClearSessions.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { Ban } from 'lucide-vue-next';
+const { pending, resetSessions } = useAccount();
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger as-child>
+      <Button variant="outline" size="icon" :disabled="pending"><Ban class="w-4 h-4" /></Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Вы уверены?</AlertDialogTitle>
+        <AlertDialogDescription>
+          Это действие не может быть отменено. Вы выйдете из аккаунта на всех устройствах, включая это. Все сессии станут невалидны.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Отмена</AlertDialogCancel>
+        <AlertDialogAction @click="resetSessions">Завершить все сессии</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/UserAccountCard.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { LogOut } from 'lucide-vue-next';
+const { userAccount, pending, logOut } = useAccount();
+</script>
+
+<template>
+  <Card v-if="userAccount">
+    <CardHeader>
+      <CardTitle class="flex flex-row items-center gap-2">
+        <div class="flex-grow">Аккаунт: <span class="font-mono">{{ userAccount.username || 'unknown' }}</span></div>
+        <TooltipProvider :delay-duration="300" disableClosingTrigger>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <Button variant="outline" size="icon" :disabled="pending" @click="logOut"><LogOut class="w-4 h-4" /></Button>
+            </TooltipTrigger>
+            <TooltipContent>Выход</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger as-child>
+              <ButtonDialogClearSessions />
+            </TooltipTrigger>
+            <TooltipContent>Завершить все сессии</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </CardTitle>
+    </CardHeader>
+  </Card>
+</template>
+
+<style lang="scss" scoped></style>
