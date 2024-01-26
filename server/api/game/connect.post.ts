e3549d671f2a77f8d87e60098589bd3cdc43b2da
SuhEugene <suheugene@ya.ru>
2024-01-26 03:10:32 +0300

    Translate errors to russian

--- server/api/game/connect.post.ts
+++ server/api/game/connect.post.ts
@@ -5,21 +5,18 @@ const TEN_HOURS = 10 * 60 * 60 * 1000;
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  if (!body.code || !body.key) return { "error": "Invalid request body" };
+  if (!body.code || !body.key) return { "error": "Неверное тело запроса. Пожалуйста, уведомьте об этом разработчика" };
 
   const byondSecret = useRuntimeConfig().byondSecret;
-  if (!body.secret || body.secret !== byondSecret) return createError({
-    statusCode: 400,
-    message: "Invalid secret"
-  });
+  if (!body.secret || body.secret !== byondSecret) return { "error": "Неверный секрет билда. Пожалуйста, уведомьте об этом разработчика." }
 
   const isThereConnection = await prisma.connectionString.count({
     where: {
-      value: body.code,
+      value: body.code.replace("-", "").toUpperCase(),
       createdAt: { gte: new Date(Date.now() - TEN_HOURS) },
     }
   });
-  if (!isThereConnection) return { "error": "Code not found" };
+  if (!isThereConnection) return { "error": "Неверный код подключения!" };
 
   console.log(`${body.key} connected to the key "${body.code}"`);
 
