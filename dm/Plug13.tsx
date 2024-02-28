7d0c6e24cc16d7401640cf310e94eb16a2a433e6
SuhEugene <suheugene@ya.ru>
2024-02-28 23:24:54 +0300

    Fix Plug13 ingame interface space and indentation

--- dm/Plug13.tsx
+++ dm/Plug13.tsx
@@ -3,6 +3,7 @@ import { useBackend, useLocalState } from '../backend';
 import { Box, Button, Input, NoticeBox, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
+
 type Plug13State = {
   is_connected: string,
   pending: boolean,
@@ -21,7 +22,7 @@ export const Plug13 = (props, context) => {
   const validateCode = (code: string) => {
     if (code.length < 10) return false;
     return codeRegex.test(code);
-  }
+  };
 
   return (
     <Window
@@ -31,42 +32,44 @@ export const Plug13 = (props, context) => {
       <Window.Content scrollable={false}>
         { !data.is_connected ? (
           <Section title="Подключение" fill>
-          <Stack direction="column" justify="center" align="center" fill>
-            { data.error ? (
-              <Stack.Item mb={1}>
-                <NoticeBox danger style={{'max-width': '300px'}}>{ data.error }</NoticeBox>
+            <Stack direction="column" justify="center" align="center" fill>
+              { data.error ? (
+                <Stack.Item mb={1}>
+                  <NoticeBox danger style={{ 'max-width': '300px' }}>{ data.error }</NoticeBox>
+                </Stack.Item>
+              ) : (<Box />)}
+              <Stack.Item>
+                <Input
+                  monospace
+                  maxLength="11"
+                  placeholder="ABCDE-FGHIJ"
+                  style={{ 'font-size': '14px', 'text-transform': 'uppercase' }}
+                  disabled={data.pending}
+                  value={data.code}
+                  onInput={(_, value) => setCode(value.toUpperCase())} />
               </Stack.Item>
-              ) : (<Box></Box>)
-            }
-            <Stack.Item>
-              <Input
-                monospace
-                maxLength="11"
-                placeholder="ABCDE-FGHIJ"
-                style={{ 'font-size': '14px', 'text-transform': 'uppercase' }}
-                disabled={data.pending}
-                value={data.code}
-                onInput={(_, value) => setCode(value.toUpperCase())}/>
-            </Stack.Item>
-            <Stack.Item mt={1}>
-              <Button
-                icon={data.pending ? "spinner" : "bolt"}
-                iconSpin={data.pending}
-                disabled={data.pending || !validateCode(code)}
-                content="Подключиться"
-                onClick={() => act('connect', { code })}/>
-            </Stack.Item>
-          </Stack>
-        </Section>
+              <Stack.Item mt={1}>
+                <Button
+                  icon={data.pending ? "spinner" : "bolt"}
+                  iconSpin={data.pending}
+                  disabled={data.pending || !validateCode(code)}
+                  content="Подключиться"
+                  onClick={() => act('connect', { code })} />
+              </Stack.Item>
+            </Stack>
+          </Section>
         ) : (
           <Section title="Статус" fill buttons={<Button icon="times" onClick={() => act('disconnect')}>Отключиться</Button>}>
             <Stack direction="column" justify="center" align="center" fill>
-              <Stack.Item>Подключен к аккаунту <b>{ data.username }</b></Stack.Item>
-              <Stack.Item mt={1} fontSize={0.9} style={{'font-style': 'italic'}}>(Это окошко можно закрыть)</Stack.Item>
+              <Stack.Item>
+                <span>Подключен к аккаунту </span>
+                <b>{ data.username }</b>
+              </Stack.Item>
+              <Stack.Item mt={1} fontSize={0.9} style={{ 'font-style': 'italic' }}>(Это окошко можно закрыть)</Stack.Item>
             </Stack>
           </Section>
         ) }
       </Window.Content>
     </Window>
-  );``
+  ); ``;
 };
