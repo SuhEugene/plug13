6c7a22f0da4dc2ccf7edcf4ead61ac53f2243976
SuhEugene <suheugene@ya.ru>
2024-02-28 23:24:28 +0300

    Add masochism emote type

--- utils/buttplugConstants.ts
+++ utils/buttplugConstants.ts
@@ -1,4 +1,4 @@
-export const allowedEmoteTypes = ['face', 'chest', 'front', 'back', 'basic'] as const;
+export const allowedEmoteTypes = ['face', 'chest', 'front', 'back', 'basic', 'masochism'] as const;
 export type AllowedEmote = (typeof allowedEmoteTypes)[number];
 
 export const allowedInteractionTypes = ['vibration', 'oscillation'] as const;
